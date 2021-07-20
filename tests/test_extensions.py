e4a74b8879471e1f91a986b4fbbc84d38e185827
Boris <b.tseytlin@lambda-it.ru>
2021-07-20 11:54:50 +0300

    Add debug output for CI

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -13,6 +13,7 @@ from dfsql.exceptions import QueryExecutionException, dfsqlException
 )
 class TestExtensions:
     def test_df_sql_simple_select(self, config, engine, csv_file):
+        print('Running with engine', engine)
         import dfsql.extensions
 
         df = engine.read_csv(csv_file)
