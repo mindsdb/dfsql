7d9d334e75cc1bdb59e6e5054e26d9fb08bc1b0e
Boris <b.tseytlin@lambda-it.ru>
2021-08-13 13:45:45 +0300

    Add CI test for package install after pypi upload

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Operating System :: POSIX :: Linux",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.7"
 )
