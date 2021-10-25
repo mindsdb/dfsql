08a8eb600bbe32e9675af5df948ba12c47ff4b1e
Boris <b.tseytlin@lambda-it.ru>
2021-10-25 18:58:27 +0300

    Version bump

--- dfsql/__about__.py
+++ dfsql/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'dfsql'
 __package_name__ = 'dfsql'
-__version__ = '0.6.6'
+__version__ = '0.6.7'
 __description__ = "SQL interface to Pandas"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
