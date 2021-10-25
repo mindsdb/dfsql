d6cb57e4670113a2f45e359215538d906d660d6e
Boris <b.tseytlin@lambda-it.ru>
2021-10-25 18:57:54 +0300

    Remove outdated base string for python 3.8 support

--- dfsql/utils.py
+++ dfsql/utils.py
@@ -1,5 +1,3 @@
-from pyparsing import basestring
-
 from dfsql.engine import pd
 from dfsql.exceptions import QueryExecutionException
 
@@ -105,34 +103,47 @@ class CaseInsensitiveKey(str):
     def __repr__(self):
         return self.key.__repr__()
 
+
 """https://stackoverflow.com/questions/2082152/case-insensitive-dictionary"""
+
+
 class CaseInsensitiveDict(dict):
     @classmethod
     def _k(cls, key):
-        return key.lower() if isinstance(key, basestring) else key
+        return key.lower() if isinstance(key, str) else key
 
     def __init__(self, *args, **kwargs):
         super(CaseInsensitiveDict, self).__init__(*args, **kwargs)
         self._convert_keys()
+
     def __getitem__(self, key):
         return super(CaseInsensitiveDict, self).__getitem__(self.__class__._k(key))
+
     def __setitem__(self, key, value):
         super(CaseInsensitiveDict, self).__setitem__(self.__class__._k(key), value)
+
     def __delitem__(self, key):
         return super(CaseInsensitiveDict, self).__delitem__(self.__class__._k(key))
+
     def __contains__(self, key):
         return super(CaseInsensitiveDict, self).__contains__(self.__class__._k(key))
+
     def has_key(self, key):
         return super(CaseInsensitiveDict, self).has_key(self.__class__._k(key))
+
     def pop(self, key, *args, **kwargs):
         return super(CaseInsensitiveDict, self).pop(self.__class__._k(key), *args, **kwargs)
+
     def get(self, key, *args, **kwargs):
         return super(CaseInsensitiveDict, self).get(self.__class__._k(key), *args, **kwargs)
+
     def setdefault(self, key, *args, **kwargs):
         return super(CaseInsensitiveDict, self).setdefault(self.__class__._k(key), *args, **kwargs)
+
     def update(self, E={}, **F):
         super(CaseInsensitiveDict, self).update(self.__class__(E))
         super(CaseInsensitiveDict, self).update(self.__class__(**F))
+
     def _convert_keys(self):
         for k in list(self.keys()):
             v = super(CaseInsensitiveDict, self).pop(k)
